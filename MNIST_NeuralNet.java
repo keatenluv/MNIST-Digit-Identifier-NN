490bb82b5af51d7598b93b92ba96e11852476e8e
keatenluv <48927041+keatenluv@users.noreply.github.com>
2022-10-25 23:58:50 -0500

    Add files via upload

--- MNIST_NeuralNet.java
+++ MNIST_NeuralNet.java
@@ -11,7 +11,7 @@ import java.io.BufferedReader;
 class Data {
 
     private int[][] oneHot = new int[1][10];
-    private double[][] pixles = new double[28][28];
+    private double[] pixles = new double[784];
     private int correctValue = 0;
 
     public void setOneHot(int n) {
@@ -23,25 +23,30 @@ class Data {
         System.out.println(Arrays.toString(oneHot[0]));
     }
 
-    public void setPixles(int i, int j, double pixleValue) {
+    public void setPixles(int i, double pixleValue) {
         double pixleNorm = pixleValue / 255;
-        pixles[i][j] = pixleNorm;
+        pixles[i-1] = pixleNorm;
         //System.out.println(i + " " + j + " " + pixles[i][j]);
     }
 
-    public void getPixleValues() {
-        for (double[] val : pixles){
+    public double[] getPixleValues() {
+        return (pixles);
+        /*for (double[] val : pixles){
             System.out.println(Arrays.toString(val));
-        }
+        }*/
     }
 
     public void Art(){
         String character = Integer.toString(correctValue);
-        for (int i = 0; i < pixles.length; i++){
-            System.out.println();
-            for (int j = 0; j < 28; j++){
-                double value = pixles[i][j];
-                if (pixles[i][j] > 0){System.out.print(character);}
+        for (int i = 0; i < 784; i++){
+            if (i % 28 != 0){
+                double value = pixles[i];
+                if (pixles[i] > 0){System.out.print(character);}
+                else{System.out.print(" ");}
+            }
+            else{
+                System.out.println();
+                if (pixles[i] > 0){System.out.print(character);}
                 else{System.out.print(" ");}
             }
         }
@@ -49,58 +54,36 @@ class Data {
 
 }
 
-// Perceptron class
-class Perceptron {
-
-    static Random r = new Random();
-
-    public static double Weight = r.nextGaussian();
-    public static double Input;
-
-    public static void main(String[] args) {
-        Input += Double.parseDouble(args[0]);
-    }
-
-    // Method to return output (true or false) of perceptron
-    public boolean output(double[][] inputs, double[][] weights) {
-
-        // Initialize sum of products
-        double SOP = 0;
-
-        // Sum the product of all inputs and weights
-        for (int i = 0; i < inputs[0].length; i++) {
-            SOP += inputs[0][i] * weights[i][0];
-        }
-
-        System.out.println(SOP + "");
-        if (SOP == 1) {
-            return false;
-        }
-        return true;
-    }
-}
 
 // Main class of the Neural Network
 class MNIST_NeuralNet {
 
+    public static Data[] trainingData = new Data[60000]; 
+    public static Double[][] weights1 = new Double[30][784];
+    public static Double[][] weights2 = new Double[10][30];
+    public static Double[][] bias = new Double[10][30];
+    
     public static void prompt() {
 
         // Greets and prompts the user
         System.out.println(
                 "\nWelcome to Keaton's Neural Network that recognizes handwritten digits. \n\n\t\tWhat would you like to do?");
-
         System.out.println();
     }
 
     public static void main(String[] args) {
-        try {setData();} catch (Exception e) {System.out.println(e);}
+        System.out.println(weights1.length);
+        //try {setData();} catch (Exception e) {System.out.println(e);}
 
-        /*
-         * for (int i = 0; i < 100; i++){
-         * // Statment to generate random number in range -1 to 1
-         * System.out.println(new Random().nextDouble(((1-(-1))))-1);
-         * }
-         */
+        // Randomize weights for first layer
+        for (int i = 0; i < weights1.length; i++){
+            weights1[i][0] = (1-(-1)*(new Random().nextDouble()))-1;
+        }
+        // Randomize weights for second layer
+        for (int i = 0; i < weights2.length; i++){
+            weights2[i][0] = (1-(-1)*(new Random().nextDouble()))-1;
+            bias[i][0] = (1-(-1)*(new Random().nextDouble()))-1;
+        }
     }
 
     public static void setData() throws Exception {
@@ -110,12 +93,13 @@ class MNIST_NeuralNet {
         fileReader = new BufferedReader(new FileReader("mnist_train.csv"));
         String line = "";
         
-        Data[] allData = new Data[10];
+        Data[] allData = new Data[60000];
 
-        /*for (int i = 0; i < 10; i++){
+        String[][] sixtyThousand = new String[60000][785];
 
-            line = fileReader.readLine();*/
-        while ((line = fileReader.readLine()) != null){
+        for (int i = 0; i < 60000; i++){
+            line = fileReader.readLine();
+        //while ((line = fileReader.readLine()) != null){
             Data currentInput = new Data();
 
             String[] values = line.split(Delimiter);
@@ -129,38 +113,42 @@ class MNIST_NeuralNet {
                     currentInput.setOneHot(Integer.parseInt(value)); 
                 }
                 else {
-                    firstIdx = (lenData-1) / 28;
-                    secondIdx = (lenData-1) % 28;
-                    currentInput.setPixles(firstIdx, secondIdx, Integer.parseInt(value));
+                    currentInput.setPixles(lenData, Integer.parseInt(value));
                     //System.out.println(lenData + " " + firstIdx + " " + secondIdx);
                 }
                 lenData += 1;
             }
 
+            trainingData[i] = currentInput;
             currentInput.Art();
+        }
 
-            //allData[i] = currentInput;
+        // Random variable
+        Random rand = new Random();
+		
+        // Randomize the order of training data
+		for (int i = 0; i < trainingData.length; i++) {
+			int randomIndexToSwap = rand.nextInt(trainingData.length);
+			Data temp = trainingData[randomIndexToSwap];
+			trainingData[randomIndexToSwap] = trainingData[i];
+			trainingData[i] = temp;
         }
 
         fileReader.close();
+
     }
 
+    public static void forwardFeed(Data input, int layer){
+        double summation = 0;
+        //input = input.getPixleValues();
+    }
 
+    public static double sigmoid(double z){
+        return (1/(1+Math.exp(-z)));
+    }
 
-    public static void create() {
-        double[][] inputs = new double[1][256];
-        double[][] weights = new double[256][1];
-
-        // Fill Weights with random variables as initial 'guess'
-        for (int i = 0; i < 256; i++) {
-
-            weights[i][0] = Math.random();
-            System.out.println("Weight " + i + ": " + weights[i][0]);
-        }
-
-        Perceptron precy = new Perceptron();
-
-        precy.output(inputs, weights);
+    public static double sigmoidPrime(double z){
+        return (sigmoid(z)*(1-sigmoid(z)));
     }
 
 }
