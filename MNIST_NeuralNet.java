c9f6fe51e06caac43123ec8ca01b00655063ab5b
keatenluv <48927041+keatenluv@users.noreply.github.com>
2022-10-26 21:55:07 -0500

    Add files via upload

--- MNIST_NeuralNet.java
+++ MNIST_NeuralNet.java
@@ -1,39 +1,45 @@
 /*
  * Name: Keaton Love
- * Description: A Neural Network made to recognize MNIST handwritten digits for CSC 475
+ * CWID: 102-57-333
+ * Assignment #2
+ * Description: A Neural Network using Stocashtic Gradient Descent and Backpropogation
+*               made to recognize MNIST handwritten digits for CSC 475
  * Due Date: 10/27
  */
 
 import java.io.*;
 import java.util.*;
 import java.io.BufferedReader;
+import java.util.stream.*;
+import java.util.Scanner;
 
+// Data class to make accessing input data easier and print ASCII art
 class Data {
 
-    private int[] oneHot = new int[10];
+    private double[] oneHot = new double[10];
     private double[] pixles = new double[784];
     private int correctValue = 0;
 
+    // Sets the value of the correct classification the digit
     public void setOneHot(int n) {
         oneHot[n] = 1;
         correctValue = n;
     }
 
-    public int[] getOneHot() {
+    // Returns the oneHot array
+    public double[] getOneHot() {
         return (oneHot);
     }
 
+    // Sets the value of pixles for the input
     public void setPixles(int i, double pixleValue) {
         double pixleNorm = pixleValue / 255;
         pixles[i-1] = pixleNorm;
-        //System.out.println(i + " " + j + " " + pixles[i][j]);
     }
 
+    // Returns all pixle values for the input
     public double[] getPixleValues() {
         return (pixles);
-        /*for (double[] val : pixles){
-            System.out.println(Arrays.toString(val));
-        }*/
     }
 
     public void Art(){
@@ -53,57 +59,172 @@ class MNIST_NeuralNet {
     public static Data[] trainingData = new Data[60000]; 
     public static Double[][] weights1 = new Double[30][784];
     public static Double[][] weights2 = new Double[10][30];
+    public static Double[] bias0 = new Double[30];
     public static Double[] bias = new Double[10];
-    
-    public static void prompt() {
-
-        // Greets and prompts the user
-        System.out.println(
-                "\nWelcome to Keaton's Neural Network that recognizes handwritten digits. \n\n\t\tWhat would you like to do?");
-        System.out.println();
-    }
+    public static boolean trained = false;
 
     public static void main(String[] args) {
         try {setData();} catch (Exception e) {System.out.println(e);}
 
+        Scanner scanner = new Scanner(System.in);
 
-        // Randomize weights for first layer
-        for (int i = 0; i < weights1.length; i++){
-            for (int j = 0; j < weights1[0].length; j++){
-                weights1[i][j] = (2*((new Random().nextDouble()))-1);
+        System.out.println(
+                "\nWelcome to Keaton's Neural Network that recognizes handwritten digits. \n\n\t\tWhat would you like to do?");
+        System.out.println();
+        /*try{
+            while (true){
+                System.out.println("\n[1] Train the network");
+                System.out.println();
+                System.out.println("\n[2] Load a pre-trained network");
+                System.out.println();
+                String input = scanner.nextLine();
+                System.out.println(input);
+                if (input == "train"){trainNetwork();}
+                else if(input == "2"){}
             }
+        } catch (Exception e){ System.out.println(e);}*/
+        trainNetwork();
+    }
+
+    // Method to print the accuracy of the network after each epoch
+    public static void printAccuracy(int[] correct, int[] all){
+        System.out.println();
+        for (int i = 0; i < correct.length; i++){
+            System.out.print(i + " = " + correct[i] + "/" + all[i]);
+            if (i == 5){System.out.println();}
         }
-        // Randomize weights for second layer
-        for (int i = 0; i < weights2.length; i++){
-            bias[i] = (2*((new Random().nextDouble()))-1);
-            for (int j = 0; j < weights2[0].length; j++){
-                weights2[i][j] = (2*((new Random().nextDouble()))-1);
-                
+        System.out.print(" Accuracy = " + Arrays.stream(correct).sum() + "/60000 " + ((Arrays.stream(correct).sum()/60000.0)*100) + "%");
+        System.out.println();
+    }
+
+    public static void loadNetwork(){
+
+    }
+
+    // Method to train the network includes SGD and Back propagation algorithims
+    public static void trainNetwork() {
+        
+        // Flag for user input menu
+        trained = true;
+
+        // Inital randomization for the networks weights and biases
+        randomizeWnB();
+
+        // Initalize 
+        double learningRate = 1.5;
+        double miniBatchSize = 10.0;
+        int epochs = 30;
+
+        // Loop for the 30 Epochs
+        for (int kms = 0; kms < epochs; kms++){
+
+            // Randomize training data
+            Randomize();
+
+            
+            int[] totalCorrect = new int[10];
+            int[] totalOfNums = new int[10];
+
+            // Contains backpropogation
+            for (int i = 0; i < 6000; i++){
+
+                // Setup variables used in SGD and backprop
+                double[] HiddenBiasGradientSum  = new double[weights1.length];
+                double[] FinalBiasGradientSum = new double[weights2.length];
+                double[][] weightGradientH = new double[weights1.length][weights1[0].length];
+                double[][] weightGradientF = new double[weights2.length][weights2[0].length];
+
+                for (int j = 0; j < 10; j++){
+                    // Hidden Layer Output and Final Layer output
+                    double[] hOutput = (forwardFeed(trainingData[(i*10)+j].getPixleValues()));
+                    double[] fOutput = (forwardFeed(hOutput));
+
+                    // Find the correct classification of input and the Networks calculated digit
+                    int maxAt = 0;
+                    int maxAt2 = 0;
+
+                    // Gets max from inputs one hot and from the networks final layer
+                    for (int idx = 0; idx < fOutput.length; idx++) {
+                        maxAt = fOutput[idx] > fOutput[maxAt] ? idx : maxAt;
+                    }
+                    for (int idx = 0; idx < trainingData[(i*10+j)].getOneHot().length; idx++) {
+                        maxAt2 = (trainingData[(i*10+j)].getOneHot())[idx] > (trainingData[(i*10+j)].getOneHot())[maxAt2] ? idx : maxAt2;
+                    }
+
+                    // Add one to totalCorrect if the networks output is correct
+                    if (maxAt2 == maxAt){
+                        totalCorrect[maxAt] += 1;
+                    }
+                    // Keeps track of total numbers
+                    totalOfNums[maxAt2] += 1; 
+
+                    // Calculate Bias Gradient for final layer
+                    double[] tempBGF = new double[10];
+                    for (int x = 0; x < weights2.length; x++){
+                        tempBGF[x] = (fOutput[x] - (trainingData[(i*10)+j].getOneHot())[x]) * fOutput[x] * (1 - fOutput[x]);
+                        FinalBiasGradientSum[x] += tempBGF[x];
+                    }
+
+                    // Calculate Bias Gradient for hidden layer
+                    double[] tempBGH = new double[30];
+                    for (int x = 0; x < weights2[0].length; x++){
+                        double sum = 0;
+                        for (int y = 0; y < weights2.length; y ++){
+                            sum += (weights2[y][x] * tempBGF[y]);
+                        }
+                        tempBGH[x] = sum * hOutput[x] * (1 - hOutput[x]);
+                        HiddenBiasGradientSum[x] += tempBGH[x];
+                    }
+
+                    // Calculate weight gradient for final layer
+                    for (int x = 0; x < weights2.length; x++){
+                        for (int y = 0; y < weights2[0].length; y++){
+                            weightGradientF[x][y] += hOutput[y] * tempBGF[x]; 
+                        }
+                    }
+
+                    // Calculate weight gradient for Hidden layer
+                    for (int x = 0; x < weights1.length; x++){
+                        for (int y = 0; y < weights1[0].length; y++){
+                            weightGradientH[x][y] += (trainingData[(i*10)+j].getPixleValues())[y] * tempBGH[x]; 
+                        }
+                    }
+
+                }
+
+                // Gradient Descent
+                // Update final layer bias
+                for (int x = 0; x < FinalBiasGradientSum.length; x++){
+                    bias[x] = bias[x] - (learningRate/miniBatchSize) * HiddenBiasGradientSum[x];
+                }
+
+                // Update hidden layer bias
+                for (int x = 0; x < HiddenBiasGradientSum.length; x++){
+                    bias0[x] = bias0[x] - (learningRate/miniBatchSize) * HiddenBiasGradientSum[x];
+                }
+
+                // Update first set of weights
+                for (int x = 0; x < weights1.length; x++){
+                    for (int y = 0; y < weights1[0].length; y++){
+                        weights1[x][y] = (weights1[x][y] - (learningRate/miniBatchSize) * weightGradientH[x][y]);
+                    }
+                }
+
+                // Update second set of weights
+                for (int x = 0; x < weights2.length; x++){
+                    for (int y = 0; y < weights2[0].length; y++){
+                        weights2[x][y] = (weights2[x][y] - (learningRate/miniBatchSize) * weightGradientF[x][y]);
+                    }
+                }
             }
+            System.out.println("Epoch: " + kms);
+            printAccuracy(totalCorrect, totalOfNums);
         }
-
-        
-
-        for (int j = 0; j < 100; j++){double[] hiddenOutput = new double[30];
-            for (int i = 0; i < 10; i++){
-                //System.out.println("Output #" + i + ":" + forwardFeed(trainingData[i].getPixleValues(), 0, 0));
-
-                double temp = forwardFeed(trainingData[i].getPixleValues(), 0, 0);
-                hiddenOutput[i] = temp;
-
-            }
-
-            System.out.println(forwardFeed(hiddenOutput, 0, 1));
-        }
-        
-        
-        
-        
     }
 
     public static void setData() throws Exception {
 
-        // Setup for the file reader
+        // Setup for the file reader to read data
         BufferedReader fileReader = null;
         final String Delimiter = ",";
         fileReader = new BufferedReader(new FileReader("mnist_train.csv"));
@@ -135,14 +256,71 @@ class MNIST_NeuralNet {
                 lenData += 1;
             }
 
+            // Add the current input's Data class to array of all data && increment index
             trainingData[index] = currentInput;
-            
             index += 1;
-            
         }
-       
 
-        // Random variable
+        // Close the file reader
+        fileReader.close();
+
+    }
+
+    // The feedforward part of the network
+    public static double[] forwardFeed(double[] input){
+        double summation = 0;
+
+        // Feedforward for first layer of the network
+        if (input.length == 784){
+            double[] layerOutput = new double[30];
+            for (int j = 0; j < 30; j++){
+                for (int i = 0; i < input.length; i++){
+                    summation += input[i] * weights1[j][i];
+                }
+                layerOutput[j] = sigmoid(summation+bias0[j]);
+            }
+            return (layerOutput);
+        }
+        // Feedforward for the second layer of the network
+        else{
+            double[] finalOutput = new double[10];
+            for (int j = 0; j < 10; j++){
+                for (int i = 0; i < input.length; i++){
+                    summation += input[i] * weights2[j][i];
+                }
+                finalOutput[j] = sigmoid(summation+bias[j]); 
+            }
+
+            return (finalOutput);
+        }
+    }
+
+    // Sigmoid activation function of a neuron
+    public static double sigmoid(double z){
+        return (1/(1+Math.exp(-z)));
+    }
+
+    public static void randomizeWnB(){
+        // Randomize weights for first layer
+        for (int i = 0; i < weights1.length; i++){
+            bias0[i] = (2*((new Random().nextDouble()))-1);
+            for (int j = 0; j < weights1[0].length; j++){
+                weights1[i][j] = (2*((new Random().nextDouble()))-1);
+            }
+        }
+        // Randomize weights for second layer
+        for (int i = 0; i < weights2.length; i++){
+            bias[i] = (2*((new Random().nextDouble()))-1);
+            for (int j = 0; j < weights2[0].length; j++){
+                weights2[i][j] = (2*((new Random().nextDouble()))-1);
+                
+            }
+        }
+    }
+
+    // Randomizes the training data
+    public static void Randomize(){
+        // Random variable  
         Random rand = new Random();
 		
         // Shuffles the order of training data
@@ -152,34 +330,6 @@ class MNIST_NeuralNet {
 			trainingData[randomIndexToSwap] = trainingData[i];
 			trainingData[i] = temp;
         }
-
-        // Close the file reader
-        fileReader.close();
-
-    }
-
-    public static double forwardFeed(double[] input, int row, int layer){
-        double summation = 0;
-        if (layer == 0){
-            for (int i = 0; i < input.length; i++){
-                summation += input[i] * weights1[row][i];
-            }
-        }
-        else {
-            for (int i = 0; i < input.length; i++){
-                summation += input[i] * weights2[row][i];
-            }
-            summation += bias[row];
-        }
-        return (sigmoid(summation));
-    }
-
-    public static double sigmoid(double z){
-        return (1/(1+Math.exp(-z)));
-    }
-
-    public static double sigmoidPrime(double z){
-        return (sigmoid(z)*(1-sigmoid(z)));
     }
 
 }
